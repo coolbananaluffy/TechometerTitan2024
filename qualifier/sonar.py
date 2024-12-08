53e5fc451347da9451ffc2f4dce0a17939c498b9
eshaan <coolbananaluffy.com>
2024-12-07 19:04:22 -0600

    :added plankton

--- qualifier/sonar.py
+++ qualifier/sonar.py
@@ -1,61 +1,62 @@
-from hub import light_matrix, motion_sensor, port
+import hub
+import motor
 import runloop
 import motor_pair
-from app import display
-import motor
+from hub import port
+from hub import motion_sensor\
 
-# declare constants
-ROTATION_TO_CM = 28 #17.2787595947
-#right attachment is port D and Left attachment is port E
-WHEELRIGHTPORT= port.F
+
+ROTATION_TO_CM = 28
+ROTATION_TO_INCH = 11
+WHEELRIGHTPORT = port.F
 WHEELLEFTPORT = port.B
 FRONTATTACMENTPORT = port.A
 REARATTACHMENTPORT = port.E
 TURNFACTOR = 1.66
 
-#####################################################################
-# Method to convert distance from inch to rotation degrees on motor
-#####################################################################
+
 def convertcmToDegree(cm):
-    return int(360/( ROTATION_TO_CM ) * cm )
+    return int(360/(ROTATION_TO_CM) * cm)
 
 
-#####################################################################
-# convert actual degree to motor specification
-#####################################################################
+def convertInchToDegree(inch):
+    return int((360/ROTATION_TO_INCH) * inch)
+
+
 def turnDegree(angle):
-    return int(( TURNFACTOR ) * angle )
+    return int((TURNFACTOR) * angle)
 
-async def main():
-    motor_pair.pair(motor_pair.PAIR_1, WHEELRIGHTPORT, WHEELLEFTPORT)
-    await motor_pair.move_tank_for_degrees(0, convertcmToDegree(10), 800 , 800)
-    await motor_pair.move_tank_for_degrees(0, turnDegree( 15 ), 200,-200)
-    await motor_pair.move_tank_for_degrees(0, convertcmToDegree(35), 800 , 800)
+
+async def plankton():
+    await motor_pair.move_tank_for_degrees(0, convertcmToDegree(-14), 500 , 500)
+    await motor_pair.move_tank_for_degrees(0, turnDegree(95), 200, -200)
+    await motor_pair.move_tank_for_degrees(0, convertcmToDegree(8), 500 , 500)
+    await motor_pair.move_tank_for_degrees(0, convertcmToDegree(-20), 500 , 500)
+    await motor_pair.move_tank_for_degrees(0, turnDegree(75), 200, -200)
+    await motor_pair.move_tank_for_degrees(0, convertcmToDegree(70), 500 , 500)
+
+async def Sonar():
     await motor_pair.move_tank_for_degrees(0, convertcmToDegree(10), 200 , 200)
-    await motor_pair.move_tank_for_degrees(0, turnDegree( 25 ), 200,-200)
-    await motor_pair.move_tank_for_degrees(0, turnDegree( -28 ), 200,-200)
-    await motor_pair.move_tank_for_degrees(0, convertcmToDegree(15), 800 , 800)
-    #krills 3,4 +coral 3 collected
-    #await motor_pair.move_tank_for_degrees(0, turnDegree( -5 ), 200,-200)
-    await motor_pair.move_tank_for_degrees(0, convertcmToDegree(8), 800 , 800)
-    await motor.run_for_degrees(FRONTATTACMENTPORT, -45, 500)
-    await motor_pair.move_tank_for_degrees(0, convertcmToDegree(10), 800 , 800)
-    await motor.run_for_degrees(FRONTATTACMENTPORT, -60, 500)
-    await motor.run_for_degrees(FRONTATTACMENTPORT, 75, 500)
-    # Move out after flipping corals
-    await motor_pair.move_tank_for_degrees(0, convertcmToDegree(-24), 800 , 800)
-    await motor_pair.move_tank_for_degrees(0, turnDegree( -45 ), 200,-200)
-    await motor.run_for_degrees(REARATTACHMENTPORT, -150, 500)
-    # Whale released
-    await motor_pair.move_tank_for_degrees(0, convertcmToDegree(-25), 800 , 800)
-    await motor_pair.move_tank_for_degrees(0, turnDegree( -28 ), 200,-200)
-    await motor_pair.move_tank_for_degrees(0, convertcmToDegree(5), 800 , 800)
-    runloop.sleep_ms(500)
-    await motor_pair.move_tank_for_degrees(0, convertcmToDegree(1), 800 , 800)
-    runloop.sleep_ms(500)
-    await motor_pair.move_tank_for_degrees(0, convertcmToDegree(-5), 800 , 800)
-    await motor_pair.move_tank_for_degrees(0, turnDegree( -120 ), 200,-200)
-    await motor_pair.move_tank_for_degrees(0, convertcmToDegree(40), 800 , 800)
-    #await motor_pair.move_tank_for_degrees(0, convertcmToDegree(5), 800 , 800)
+    #runloop.sleep_ms(300)
+    await motor_pair.move_tank_for_degrees(0, turnDegree(-15), 200, -200)
+    #runloop.sleep_ms(250)        
+    await motor_pair.move_tank_for_degrees(0, convertcmToDegree(70), 500 , 500)
+    await motor_pair.move_tank_for_degrees(0, turnDegree(3), 200, -200)
+    await motor_pair.move_tank_for_degrees(0, convertcmToDegree(10), 500 , 500)
+    #
+    await motor.run_for_degrees(REARATTACHMENTPORT,400,500)
+    await motor.run_for_degrees(REARATTACHMENTPORT,-2000,500)
+
+
+
+
+    
+    
+async def main():
+    # Set Motor pair1
+    motor_pair.pair(motor_pair.PAIR_1, WHEELRIGHTPORT, WHEELLEFTPORT)
+    await Sonar()
+    await plankton()
+    #await test()
 
 runloop.run(main())
