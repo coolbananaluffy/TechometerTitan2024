9cf5a6647627ee1ac833602efe0f77323606fa04
anithavarghese <anithavarghese83@gmail.com>
2025-02-02 11:48:39 -0600

    Update route11_RT.py

--- RegionalEvent8Feb/route11_RT.py
+++ RegionalEvent8Feb/route11_RT.py
@@ -1,9 +1,8 @@
-from hub import light_matrix, motion_sensor, port
+from hub import light_matrix, motion_sensor, port, sound
 import runloop
 import motor_pair
-#from app import display
+from app import display
 import motor
-import color_matrix
 
 # declare constants
 ROTATION_TO_CM = 28 #17.2787595947
@@ -41,49 +40,18 @@ def turnDegree(angle):
 async def main():
     # Set Motor pair1
     motor_pair.pair(motor_pair.PAIR_1, WHEELRIGHTPORT, WHEELLEFTPORT)
-    await motor.run_for_degrees(REARATTACHMENTPORT,50,500)
-    await motor_pair.move_tank_for_degrees(0,convertcmToDegree(48), 500, 500)
 
-    await motor_pair.move_tank_for_degrees(0, turnDegree(-47), 200, -200)
-    runloop.sleep_ms(1000)
-
-    await motor_pair.move_tank_for_degrees(0,convertcmToDegree(63.5), 500, 500)
-
-    await motor.run_for_degrees(REARATTACHMENTPORT,-800,500)
-
-    await motor_pair.move_tank_for_degrees(0,convertcmToDegree(-17), 500, 500)
-
-    await motor_pair.move_tank_for_degrees(0, turnDegree(-49), 200, -200)
-
-    await motor.run_for_degrees(REARATTACHMENTPORT,500,500)
-
-    await motor_pair.move_tank_for_degrees(0,convertcmToDegree(38), 500, 500)
-    await motor_pair.move_tank_for_degrees(0,convertcmToDegree(-10), 500, 500)
-
-    await motor_pair.move_tank_for_degrees(0, turnDegree(30), 200, -200)
-
-    await motor_pair.move_tank_for_degrees(0, convertcmToDegree(30), 500 , 500)
-
-    await motor.run_for_degrees(REARATTACHMENTPORT,-60,1000)
-    await motor_pair.move_tank_for_degrees(0, turnDegree(80), 100, -100)
-    #runloop.sleep_ms(3000)
-    await motor_pair.move_tank_for_degrees(0, turnDegree(270), 1000, -1000)
-    await motor_pair.move_tank_for_degrees(0, turnDegree(90), 1000, -1000)
-    await motor_pair.move_tank_for_degrees(0, convertcmToDegree(40), 500 , 500)
-    await motor_pair.move_tank_for_degrees(0, turnDegree(-90), 1000, -1000)
-    await motor_pair.move_tank_for_degrees(0, convertcmToDegree(40), 500 , 500)
-    #await motor_pair.move_tank_for_degrees(0, convertcmToDegree(-3), 500 , 500)
-    #await motor.run_for_degrees(REARATTACHMENTPORT, 100,1000)
-    #await motor_pair.move_tank_for_degrees(0, turnDegree(-90), 100, -100)    
-    #await motor_pair.move_tank_for_degrees(0, convertcmToDegree(10), 500 , 500)
-'''
-    await motor.run_for_degrees(REARATTACHMENTPORT,1000,1000)
-
-    await motor.run_for_degrees(REARATTACHMENTPORT,-65,1000)
-
-    await motor_pair.move_tank_for_degrees(0, turnDegree(500), 1000, -1000)
-'''
-
-
+    #await motor_pair.move_tank_for_degrees(0, convertcmToDegree(5), 800 , 800)
+    #await motor.run_for_degrees(FRONTATTACMENTPORT,100,250)
+    #await motor_pair.move_tank_for_degrees(0, turnDegree(60), 200, -200)
+    #await motor_pair.move_tank_for_degrees(0, convertcmToDegree(24), 800 , 800)
+    #await motor_pair.move_tank_for_degrees(0, turnDegree(15), 200, -200)
+    #await motor.run_for_degrees(REARATTACHMENTPORT,90,250)    
+    await motor_pair.move_tank_for_degrees(0, convertcmToDegree(50), 1000, 1000)    
+    await motor_pair.move_tank_for_degrees(0, convertcmToDegree(10), 200, 200)
+    await motor.run_for_degrees(REARATTACHMENTPORT,-60,200)
+    #await motor_pair.move_tank_for_degrees(0, convertcmToDegree(4), 100, 100)
+    runloop.sleep_ms(4000)
+    await motor_pair.move_tank_for_degrees(0, convertcmToDegree(-70), 1000 , 1000)    
 
 runloop.run(main())
